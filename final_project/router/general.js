0702cb36507c38f6dc898dcc7139ea1b77123342
jmill29 <jacoblmiller.jlm@gmail.com>
2024-04-23 15:40:29 -0400

    Added endpoint to retrieve book details by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,15 +12,26 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  // Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  if (req.params.isbn) {
+    const bookToSend = books[req.params.isbn];
+    if (bookToSend) {
+      res.status(200).send(JSON.stringify(books[req.params.isbn], null, 4));
+    } else {
+      res.status(404).send(JSON.stringify({
+        message: "Book not found."
+      }, null, 4));
+    }
+  } else {
+    res.status(400).send(JSON.stringify({
+      message: "Please specify the isbn of the book you'd like to get"
+    }, null, 4));
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
