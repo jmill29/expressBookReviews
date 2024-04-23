bb434b96bf67497de1ad4d7f64080f5377f3af2a
jmill29 <jacoblmiller.jlm@gmail.com>
2024-04-22 21:30:42 -0400

    Added middleware to retrieve list of available books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,20 +13,21 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+  // Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+public_users.get('/author/:author', function (req, res) {
+  // Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
