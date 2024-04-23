d23413697e8bc7743b24ec779d7268e59ceff124
jmill29 <jacoblmiller.jlm@gmail.com>
2024-04-23 17:35:51 -0400

    Added endpoint to retrieve books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -53,8 +53,20 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let booksWithTitle = {};
+  for (let book in books) {
+    if (books[book].title.includes(title)) {
+      booksWithTitle[book] = books[book];
+    }
+  }
+  if (Object.keys(booksWithTitle).length > 0) {
+    res.status(200).send(JSON.stringify(booksWithTitle, null, 4));
+  } else {
+    res.status(404).send(JSON.stringify({
+      message: "No books found."
+    }, null, 4));
+  }
 });
 
 //  Get book review
