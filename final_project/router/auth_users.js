f89ed89e43df659717e2692db7841c1ce07aaf21
jmill29 <jacoblmiller.jlm@gmail.com>
2024-04-23 20:54:19 -0400

    Implemented user login functionality with error handling and authentication

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,47 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  for (let user of users) {
+    if (user.username === username) {
+      return false;
+    }
+  }
+  return true;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  for (let user of users) {
+    if (user.username === username && user.password === password) {
+      return true;
+    }
+  }
+  return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).send(JSON.stringify({
+      message: "Unable to login. Please include both a username and a password."
+    }, null, 4));
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+      data: username
+    }, 'user_access', { expiresIn: 60 * 60 });
+
+    req.session.auth = {
+      accessToken,username
+    };
+
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(400).send("Invalid Login. Check username and password.");
+  }
 });
 
 // Add a book review
